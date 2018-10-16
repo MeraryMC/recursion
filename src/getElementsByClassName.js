e61cdbaf0fb96bb5362f4ee744cd9a22300ce25f
Merary Martinez-Cobian <40481640+MeraryMC@users.noreply.github.com>
2018-10-15 20:00:30 -0700

    Completed - stringifyJSON & getElementsByClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,20 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+	var classNameElements = [];
+	var htmlBody = document.body;
+	var checkForClassName = function(htmlBody) {
+		if (htmlBody.classList && htmlBody.classList.contains(className)) {
+			classNameElements.push(htmlBody);
+		}
+		if(htmlBody.hasChildNodes()) {
+			for (var i = 0; i < htmlBody.childNodes.length; i++) {
+				checkForClassName(htmlBody.childNodes[i]);
+			}
+		}
+
+	}
+	checkForClassName(htmlBody);
+	return classNameElements;
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,38 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  var arrayOfValues = [];
+  if (obj === null || obj === undefined ) {
+    return 'null'
+  }
+  if (typeof(obj) === 'number' || typeof(obj) === 'symbol' || typeof obj === 'boolean') {
+    return String(obj)  
+  }
+  if (typeof obj === 'string') {
+    return '"' + obj + '"'
+  }
+  if(Array.isArray(obj)) {
+    if (obj.length === 0) {
+      return '[]';
+    }
+    for (var i = 0; i < obj.length; i++) {
+      arrayOfValues.push(stringifyJSON(obj[i]));
+    }
+    return '[' + arrayOfValues + ']';
+  }
+
+  if (obj instanceof Object) {
+    for (var key in obj) {
+      if(obj[key] instanceof Function) {
+        return '{}';
+      } else {
+        arrayOfValues.push(stringifyJSON(key) + ':' + stringifyJSON(obj[key]));
+      }
+    }
+    return ( '{' + arrayOfValues + '}');
+  }
 };
+
+ 
+
+
